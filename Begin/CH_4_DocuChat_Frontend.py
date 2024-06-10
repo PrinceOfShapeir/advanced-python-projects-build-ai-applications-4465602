572ab856ccd1782ed9dea9896f8f593472d0c9e8
Sean Farley <somethingfarley@gmail.com>
2024-06-10 07:57:42 +0000

    ch4

--- Begin/CH_4_DocuChat_Frontend.py
+++ Begin/CH_4_DocuChat_Frontend.py
@@ -158,7 +158,7 @@ if data_file is not None:
             # Simulate stream of response with milliseconds delay
             for chunk in assistant_response.split():
                 full_response += chunk + " "
-                time.sleep(0.05)
+                time.sleep(0.1)
 
                 # Add a blinking cursor to simulate typing
                 message_placeholder.markdown(full_response + "▌")
